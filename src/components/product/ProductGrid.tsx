d233b3d3403014b99bd18c92d4101a08bd635ed6
rong5026 <rong5026@naver.com>
2025-07-12 23:51:31 +0900

    feat: 상품 그리드 레이아웃

---
+++ src/components/product/ProductGrid.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import ProductCard from './ProductCard'
+import { Product } from './product.types'
+
+// ProductGrid Props 타입 정의
+interface ProductGridProps {
+  products: Product[]
+  className?: string
+  onProductClick?: (product: Product) => void
+  onHeartClick?: (product: Product) => void
+}
+
+// ProductGrid 컴포넌트
+const ProductGrid: React.FC<ProductGridProps> = ({
+  products,
+  className = '',
+  onProductClick,
+  onHeartClick,
+}) => {
+  return (
+    <div className={`grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 ${className}`}>
+      {products.map((product) => (
+        <ProductCard
+          key={product.productNumber}
+          product={product}
+          showBrand={true}
+          showPreviousPrice={true}
+          showNotificationCount={true}
+          buttonText={'알림 등록하기'}
+          buttonIcon={'bell-plus'}
+          onProductClick={onProductClick}
+          onHeartClick={onHeartClick}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default ProductGrid
